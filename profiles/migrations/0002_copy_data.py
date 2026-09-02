cee6d7e95a4d430708813afb5b424872cd3157e6
Tom Mouquet <mouquettom@gmail.com>
2026-09-02 10:10:21 +0200

    Refactor Django app architecture

---
+++ profiles/migrations/0002_copy_data.py
@@ -0,0 +1,26 @@
+from django.db import migrations
+
+
+def copy_profiles_data(apps, schema_editor):
+
+    OldProfile = apps.get_model('oc_lettings_site', 'Profile')
+    NewProfile = apps.get_model('profiles', 'Profile')
+
+    for old_profile in OldProfile.objects.all():
+        NewProfile.objects.create(
+            id=old_profile.id,
+            user_id=old_profile.user_id,
+            favorite_city=old_profile.favorite_city,
+        )
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('oc_lettings_site', '0002_auto_20260819_0849'),
+        ('profiles', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RunPython(copy_profiles_data),
+    ]
